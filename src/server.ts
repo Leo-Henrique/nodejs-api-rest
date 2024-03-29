3c6acf844862e61e129fbae9dfb59a29a05fb11c
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-03-29 18:19:12 -0300

    chore(server): update basic setup

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,8 @@ import { app } from "./app";
 import { env } from "./env";
 
 (async () => {
-  await app.listen({ host: "0.0.0.0", port: env.APP_PORT });
+  const serverName = await app.listen({ port: env.APP_PORT });
 
-  console.log(
-    `Application "${env.APP_NAME}" HTTP server is running on port ${env.APP_PORT}!`,
-  );
+  console.log(`Application "${env.APP_NAME}" is running!`);
+  console.log(serverName);
 })();
